00bbcd4ee0d9f2b1ea347d6190148ef52d7bfa1c
Bouarguan Abdellah <90093994+AbdellahBouarguan@users.noreply.github.com>
2022-07-02 22:59:51 +0100

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/abdo/Desktop/Cpp/projects/flappyBird
-# Build directory: /home/abdo/Desktop/Cpp/projects/flappyBird/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
